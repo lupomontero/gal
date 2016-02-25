8414500220a6b79d9061d5ec6145f15ee01b3c56
Lupo Montero <lupomontero@gmail.com>
2016-02-25 12:38:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gal
+
+## Options
+
+* `PORT`: Port to bind to.
+* `JWT`: Path to json file with jwt credentials.
+* `JWT_EMAIL`: If not using `JWT` you can specify JWT client email along with
+  `JWT_PRIVATE_KEY`.
+* `JWT_PRIVATE_KEY`: String with private key. Used with `JWT_EMAIL`.
+* `ADMINS`: Comma separated lists of admin emails or path to json file with array of admin emails.
+* `NAME`: Shared folder name.
---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Images = require('./lib/images');
+const Routes = require('./lib/routes');
+
+
+const server = new Hapi.Server({
+  app: {
+    port: process.env.PORT || 3000,
+    admins: (function () {
+
+      const admins = (process.env.ADMINS || '').split(',').map((admin) => {
+
+        return admin.trim();
+      });
+
+      if (!admins || !admins.length) {
+        throw new Error('No admins defined!');
+      }
+
+      if (admins.length === 1 && /\.json$/.test(admins[0])) {
+        return require(admins[0]);
+      }
+
+      return admins;
+    }()),
+    jwt: (function () {
+
+      if (process.env.JWT) {
+        return require(process.env.JWT);
+      }
+      return {
+        client_email: process.env.JWT_EMAIL,
+        private_key: process.env.JWT_PRIVATE_KEY
+      }
+    }())
+  }
+});
+
+server.connection({ port: server.settings.app.port });
+
+server.register([Inert, Images], (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.route(Routes);
+
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+
+    console.log('Server running at:', server.info.uri);
+  });
+});
